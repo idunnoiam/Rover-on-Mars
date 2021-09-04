35a1c6796b23d759ea6f42fc7f6de2fd5b3c70fa
idunnoiam <74386219+idunnoiam@users.noreply.github.com>
2021-09-03 22:11:25 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+rover_width = 100;
+rover_height = 90;
+rover_x = 10;
+rover_y = 10;
+background_image = "mars.jpg";
+rover_img = "rover.png";
+
+
+function add() {
+    background_image_tag = new Image();
+    background_image_tag.onload = uploadBackground;
+    background_image_tag.src = background_image;
+
+    rover_imgTag = new Image();
+    rover_imgTag.onload = uploadrover;
+    rover_imgTag.src = rover_img;
+}
+
+function uploadBackground() {
+    ctx.drawImage(background_image_tag, 0, 0, canvas.width, canvas.height);
+
+}
+
+function uploadrover() {
+    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if (keyPressed == '38') {
+        up();
+        console.log("up");
+    }
+    if (keyPressed == '37') {
+        left();
+        console.log("left");
+    }
+    if (keyPressed == '39') {
+        right();
+        console.log("right");
+    }
+    if (keyPressed == '40') {
+        down();
+        console.log("down");
+    }
+}
